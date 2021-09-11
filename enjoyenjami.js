403f30b3d539b97f902f3b672fe185226fa10224
durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2021-09-11 11:52:42 +0530

    Add files via upload

--- enjoyenjami.js
+++ enjoyenjami.js
@@ -1,4 +1,21 @@
+var tts_sqaud="";
+function preload() {
+    durvish=loadSound("music.mp3.icloud")
+}
 function setup() {
-    canvas=createCanvas(550,551)
-    canvas.center();
+    canvas=createCanvas(550,550);
+    canvas.position(400,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    hp=ml5.poseNet(video,optimus);
+    hp.one("pose",gotposes);
+}
+function optimus(){
+    console.log("model is loaded");
+}
+function draw(){
+    image(video,0,0,550,550);
+}
+function as(){
+    tts_sqaud.play();
 }
